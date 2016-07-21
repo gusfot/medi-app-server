57be1b510337fe5f2e69258eaa15ddfcca3bd810
hyunlae.kim <hyunlae.kim@ohjic.com>
2016-07-21 11:36:17 +0900

    주석추가

--- src/main/java/org/ohjic/flower/exception/PermissionDeniedException.java
+++ src/main/java/org/ohjic/flower/exception/PermissionDeniedException.java
@@ -3,7 +3,15 @@ package org.ohjic.flower.exception;
 import org.ohjic.flower.exception.common.CommonException;
 import org.ohjic.flower.exception.common.ResponseCode;
 
+/**
+ * 권한이 없을 경우 예외처리
+ * @author ohjic
+ *
+ */
 public class PermissionDeniedException extends CommonException {
+	
+	private static final long serialVersionUID = 1L;
+
 	public PermissionDeniedException() {
 		super(ResponseCode.PERMISSION_DENIED);
 	}
