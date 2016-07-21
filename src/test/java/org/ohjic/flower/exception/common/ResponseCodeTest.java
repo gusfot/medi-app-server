5bab13aea7ffb768501bf183dff157c9ee044947
hyunlae.kim <hyunlae.kim@ohjic.com>
2016-07-21 12:59:01 +0900

    Json 응답객체테스트

---
+++ src/test/java/org/ohjic/flower/exception/common/ResponseCodeTest.java
@@ -0,0 +1,33 @@
+package org.ohjic.flower.exception.common;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ResponseCodeTest {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ResponseCodeTest.class);
+			
+	@Test
+	public void testSuccess() {
+		
+		ResponseCode resCode = ResponseCode.SUCCESS;
+		assertTrue(resCode.getCode().equals("SUCC00"));
+	}
+	
+	@Test
+	public void testUnkown() {
+		
+		ResponseCode resCode = ResponseCode.UNKOWN;
+		assertTrue(resCode.getCode().equals("ERR0000"));
+	}
+	
+	@Test
+	public void testMessageByEnum() {
+		
+		String message = ResponseCode.getMessage(ResponseCode.PERMISSION_DENIED);
+		logger.debug(message);
+	}
+}
