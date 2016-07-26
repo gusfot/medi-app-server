551320867c62316feecb273a08c2c66aaa2ef38d
hyunlae.kim <hyunlae.kim@ohjic.com>
2016-07-26 11:35:36 +0900

    게시판 샘플

---
+++ src/main/java/org/ohjic/flower/service/impl/BoardServiceImpl.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package org.ohjic.flower.service.impl;
+
+import org.ohjic.flower.dao.BoardMapper;
+import org.ohjic.flower.model.Board;
+import org.ohjic.flower.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author ohjic
+ *
+ */
+@Service
+public class BoardServiceImpl implements BoardService {
+
+	@Autowired
+	BoardMapper boardMapper;
+	
+	/* (non-Javadoc)
+	 * @see org.ohjic.flower.service.BoardService#regist(org.ohjic.flower.model.Board)
+	 */
+	@Override
+	public boolean regist(Board board) {
+		// TODO Auto-generated method stub
+		return boardMapper.insert(board) == 1;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ohjic.flower.service.BoardService#getBoard(int)
+	 */
+	@Override
+	public Board getBoard(int boardSeq) {
+		// TODO Auto-generated method stub
+		return boardMapper.selectByPrimaryKey(boardSeq);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ohjic.flower.service.BoardService#modify(org.ohjic.flower.model.Board)
+	 */
+	@Override
+	public boolean modify(Board board) {
+		// TODO Auto-generated method stub
+		return boardMapper.updateByPrimaryKey(board) == 1;
+	}
+
+}
---
+++ src/main/java/org/ohjic/flower/web/BoardController.java
@@ -0,0 +1,23 @@
+package org.ohjic.flower.web;
+
+import org.ohjic.flower.model.Board;
+import org.ohjic.flower.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BoardController {
+
+	@Autowired
+	BoardService boardService;
+	
+	@RequestMapping(value = "/board/regist", method=RequestMethod.POST)
+	public String regist(@ModelAttribute Board board) {
+		boardService.regist(board);
+				
+		return "detail/"+board.getBoardSeq();
+	}
+}
